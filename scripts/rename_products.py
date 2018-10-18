d0b6990ed56f1cfdac6e1663e2fc1af505f82b51
chrishah <chrisi.hahni@gmail.com>
2018-10-18 16:33:06 +0200

    update scripts

---
+++ scripts/rename_products.py
@@ -0,0 +1,69 @@
+#! /usr/bin/python
+
+import sys
+## The script renames product names according to a tsv file
+## usage: script.py new_names.tsv original.gff > new.gff
+
+newnames=sys.argv[1]
+
+
+fh = open(newnames)
+
+names={}
+
+for line in fh:
+	temp = line.strip().split("\t")
+	names[temp[0]] = {'old':temp[1], 'new':temp[2]}
+
+fh.close()
+
+#for gene in sorted(names):
+#	print gene,names[gene]
+
+fh=open(sys.argv[2])
+
+for line in fh:
+	line = line.strip()
+	if len(line.strip().split("\t")) == 9:
+		if line.strip().split("\t")[2] == 'gene':
+			found=0
+			temp = line.strip().split("\t")
+			features = temp[-1].split(";")
+			for feat in features:
+				if feat.startswith("ID="):
+					gene_ID = feat.replace("ID=","")
+					break
+
+#			print gene_ID
+			
+			if gene_ID in names: #searching for the gene by name
+				found=1
+				print "FOUND"
+				for i in reversed(range(len(features))):
+					if features[i].startswith("description="):
+#						print "Replacing %s with %s" %(features[i].replace("description=",""), names[gene_ID]['new'])
+						if not names[gene_ID]['new'] == '-':
+							features[i] = "description=%s" %names[gene_ID]['new']
+						else:
+							del(features[i])
+						break
+
+				feats = ";".join(features)
+				temp[-1] = ";".join(features)
+				line = "\t".join(temp)
+
+		if line.strip().split("\t")[2] == 'CDS' and found:
+			temp = line.strip().split("\t")
+                        features = temp[-1].split(";")
+			for i in reversed(range(len(features))):
+				if features[i].startswith("product="):
+					if not names[gene_ID]['new'] == '-':
+						features[i] = "product=%s" %names[gene_ID]['new']
+					else:
+						del(features[i])
+					break
+			feats = ";".join(features)
+                        temp[-1] = ";".join(features)
+                        line = "\t".join(temp)
+
+	print line
--- scripts/transfer_annotations_from_blast2go_go_table_to_maker2gff.py
+++ scripts/transfer_annotations_from_blast2go_go_table_to_maker2gff.py
@@ -20,10 +20,61 @@ for line in fh.readlines()[1:]:
 	ID=l[0]
 	dictionary[ID] = {'product':'', 'GO':'', 'ec_number':'', 'IPR':'', 'Dbxref':''}
 	dbxref_list = []
-	if '--NA--' in l[2] or 'LOC' in l[2] or 'hypothetical protein' in l[2]: #'--NA--' seems to be used when there was no significant blast hit; Some descriptions indicate similarity to uncharacterized proteins with an ID starting with LOC.... (uncharacterized proteins from a variety of organisms in the database) or to some hypothetical proteins from specific organisms. These cases are ignored by the script so the gene in question won't receive a functional annotation in the gff file 
-		pass
+
+#Weed out problematic product descriptions
+
+#These cases are ignored by the script so the gene in question won't receive a functional annotation in the gff file 
+#'--NA--' seems to be used when there was no significant blast hit;
+#Some descriptions indicate similarity to uncharacterized proteins with an ID starting with LOC.... (uncharacterized proteins from a variety of organisms in the database)
+#hypothetical proteins from specific organisms.
+#descriptions containing 'unnamed'
+#Low quality protein causes a fatal error
+#anything containing uncharacterized or uncharacterised I use 'ncharacteri' as the common pattern
+#description containing unkown - 'nknown'
+	exceptions = ['--NA--', 'LOC', 'hypothetical protein', 'Hypothetical protein', 'unnamed', 'LOW QUALITY PROTEIN', 'ow quality protein', 'ncharacteri','nknown']
+	if l[2].isdigit(): #if the description contains only of numbers
+		l[2] = '' #reset the description to nothing
 	else:
-		dictionary[ID]['product'] = l[2]
+		for case in exceptions: #if one of the exceptions is true
+			if case in l[2]:
+				l[2] = '' #reset the description to nothing
+				break
+	
+	if l[2]: #if there is something left
+		if 'partial' in l[2]:
+			l[2] = l[2].replace("partial", "") #according to the annotation guidelnies the word partial is not allowed and will thus be removed, in the descriptions produced by Blast2GO 'partial' just indicates that it matched to a partial gene as far as I understand
+
+		#check for any weird characters at the beginning of the description
+		ok = 0
+		while not ok: #the description should come out without any strange things at the start of this while loop
+			if l[2].startswith(" "):
+				l[2] = l[2].lstrip() # lstrip removes any spaces at the beginning of the string
+			elif l[2].startswith('-'): # sometimes the description starts with a hyphen which causes problems - these are removed
+				l[2] = l[2][:-1]
+			elif l[2].startswith('['):
+				l[2] = l[2][:-1] # sometimes the description starts with a '[' which causes problems - these are removed
+			elif l[2].startswith(','):
+				l[2] = l[2][:-1]
+			else:
+				ok = 1
+
+		#check for any weird characters at the end of the description
+		ok = 0
+		while not ok: #the description should come out without any strange things at the end of this while loop
+			if l[2].endswith(" "):
+				l[2] = l[2].rstrip() # rstrip removes any spaces at the end of the string
+			elif l[2].endswith('-'): # sometimes the description ends with a hyphen which causes problems - these are removed
+				l[2] = l[2][:-1]
+			elif l[2].endswith('['):
+				l[2] = l[2][:-1] # sometimes the description ends with a '[' which causes problems - these are removed
+			elif l[2].endswith(','):
+				l[2] = l[2][:-1]
+			else:
+				ok = 1
+
+		if l[2]: #if there's something left after the above steps
+			dictionary[ID]['product'] = l[2] 
+
 	if len(l) >= 9:
 		GOs = l[8].split("; ")
 		for i in range(len(GOs)):
@@ -31,7 +82,14 @@ for line in fh.readlines()[1:]:
 		
 		dictionary[ID]['GO'] = ",".join(GOs)
 	if len(l) >= 11:
-		dictionary[ID]['ec_number'] = ":".join(l[10].split(":")[1:])
+#		print l
+		if l[10].startswith("EC"):
+			ECs = l[10].split("; ")
+			final_ECs = []
+			for i in reversed(range(len(ECs))):
+				if len(ECs[i].split(":")[1].split(".")) == 4: #The NCBI submission only accepts EC numbers that are in the format x.x.x.x (so 4 numbers separated by '.') -> x.x.x sometimes occurs in Blast2GO, but these will be excluded
+					final_ECs.append(ECs[i].split(":")[1])
+			dictionary[ID]['ec_number'] = ",".join(final_ECs)
 	if len(l) >= 13:
 		if not l[12] == 'no IPS match':
 			IPRs = l[12].split("; ")
@@ -41,9 +99,13 @@ for line in fh.readlines()[1:]:
 				if IPRs[i].startswith('IPR'):
 					final_IPRs.append("InterPro:%s" %IPRs[i].split(" ")[0])
 				else:
-					if not 'COILS' in IPRs[i]:
-						db=IPRs[i].split(" ")[1].replace("(","").replace(")","")
-						final_IPRs.append("%s:%s" %(db,IPRs[i].split(" ")[0]))
+					if 'PIRSF' in IPRs[i]:
+						final_IPRs.append("PIR:%s" %IPRs[i].split(" ")[0])
+					else:
+						pass # other databases like GENE3D,HAMAP,PANTHER,PRINTS,PRODOM,PROSITE_PATTERNS,PROSITE_PROFILES,SMART,SUPERFAMILY seem not to be supported by NCBI and throw an error when trying to submit the annotation 
+#					if not 'COILS' in IPRs[i]:
+#						db=IPRs[i].split(" ")[1].replace("(","").replace(")","")
+#						final_IPRs.append("%s:%s" %(db,IPRs[i].split(" ")[0]))
 
 
 #			print final_IPRs
@@ -66,7 +128,10 @@ for line in fh.readlines()[1:]:
 for line in open(gff):
 	line=line.strip()
 	if "\tgene\t" in line:
+		pseudo = 0
 		ID = line.split("\t")[8].split(";")[0].split("=")[1]
+		if 'pseudo=true' in line.strip():
+			pseudo = 1
 		found=0
 		for ids in dictionary.keys():
 			if ID == "-".join(ids.split("-")[:-2]):
@@ -80,14 +145,19 @@ for line in open(gff):
 				break
 		if not found:
 			print line
+
 	elif "\tCDS\t" in line:
 #		print line
-		mRNA_ID = line.split("\t")[8].split(";")[0].split("=")[1].split(":")[0]
-#		print mRNA_ID,dictionary[mRNA_ID]
+		if not pseudo:
+			mRNA_ID = line.split("\t")[8].split(";")[0].split("=")[1].split(":")[0]
+#			print mRNA_ID,dictionary[mRNA_ID]
 
-		for qual in ['product', 'Dbxref', 'ec_number']:
-			if dictionary[mRNA_ID][qual]:
-				line += ";%s=%s" %(qual, dictionary[mRNA_ID][qual])
+			for qual in ['product', 'Dbxref']: #add product name and Evidence codes from GO and IPR to the CDS if any exist
+				if dictionary[mRNA_ID][qual]:
+					line += ";%s=%s" %(qual, dictionary[mRNA_ID][qual])
+			if dictionary[mRNA_ID]['product'] and dictionary[mRNA_ID]['ec_number']: #only add the EC number if the protein has a valid product name
+				line += ";ec_number=%s" %dictionary[mRNA_ID]['ec_number']
+
 		print line
 	else:
 		print line
